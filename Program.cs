a1683bd90d8c1ec798e192187be5ac493fe861ea
valery <valeradoroshkov987@gmail.com>
2022-04-06 20:31:51 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+namespace Project_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("First project");
+        }
+    }
+}
